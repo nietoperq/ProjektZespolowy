12c78c934fcf41428845180470dc20e902d5f323
ZuzKop <bB14RXVWcNkv>
2020-11-26 17:45:23 +0100

    Nowa scena (ekran tutułowy)
    Ekran główny, nie funkcjonalny

---
+++ Assets/Scripts/onHoverChangeText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class onHoverChangeText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text myText;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myText.text = "- Start -";
+        myText.fontSize = 58;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myText.text = "Start";
+        myText.fontSize = 52;
+    }
+}
---
+++ Assets/Scripts/onHoverChangeText1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class onHoverChangeText1 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text myText;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myText.text = "- About the game -";
+        myText.fontSize = 58;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myText.text = "About the game";
+        myText.fontSize = 52;
+    }
+}
