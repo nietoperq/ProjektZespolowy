5479c13428cbb10bc716e2a5bed035aff4a832a1
vshchq <62289858+vshchq@users.noreply.github.com>
2020-11-28 00:15:29 +0100

    Mechanika doznawania obrazen + particles test
    Kod jeszcze poprawie, ale działać działa. Jak skoncze pracowac nad checkpointami, bo na razie to testuje w innym projekcie, to dodam umieranie i respawn

---
+++ Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    public int damageToGive = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Vector3 knockbackDirection = other.transform.position - transform.position;
+            knockbackDirection = knockbackDirection.normalized;
+            FindObjectOfType<HealthManager>().HurtPlayer(damageToGive, knockbackDirection);
+        }
+    }
+}
